a26078f5a84b53a65e1677ccb981372961e290ee
AgusCason <88736454+AgusCason@users.noreply.github.com>
2022-08-28 23:55:33 -0300

    Add files via upload

---
+++ class.js
@@ -0,0 +1,11 @@
+
+
+class DatosUsuarios {
+    constructor(nombre, apellido, usuario, email, contraseña) {
+        this.nombre = nombre ;
+        this.apellido = apellido ;
+        this.usuario = usuario ;
+        this.email = email ;
+        this.contraseña = contraseña ;
+    }
+}
