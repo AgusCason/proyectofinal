6bf4256a302e612aac80760c72a144e319f62e20
AgusCason <88736454+AgusCason@users.noreply.github.com>
2022-09-18 20:35:09 -0300

    Add files via upload

--- js/usuarios.js
+++ js/usuarios.js
@@ -92,27 +92,6 @@ function leerUsuarios (){
 }
 
 
-const imgUsUno = document.getElementById('usUno') ;
-const imgUsDos = document.getElementById('usDos') ;
-const imgUsTres = document.getElementById('usTres') ;
-const imgUsCuatro = document.getElementById('usCuatro') ;
-let us = 0 ;
 
-imgUsUno.addEventListener('click', () => {
-    let us = 10 ;
-    localStorage.setItem("us", us) ;
-})
-imgUsDos.addEventListener('click', () => {
-    let us = 20 ;
-    localStorage.setItem("us", us) ;
-})
-imgUsTres.addEventListener('click', () => {
-    let us = 30 ;
-    localStorage.setItem("us", us) ;
-})
-imgUsCuatro.addEventListener('click', () => {
-    let us = 40 ;
-    localStorage.setItem("us", us) ;
-})
 
 
